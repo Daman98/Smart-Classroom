748abd57a0a674f5b86dfa0b3d12d81e6d05b87a
Apurva N. Saraogi <apurva9@iitg.ernet.in>
2018-11-19 12:37:49 +0530

    Presentation Done

--- Glove Pointer/mouse.ino
+++ Glove Pointer/mouse.ino
@@ -1,21 +1,8 @@
-/*
-Code of Arduino to Control Mouse pointer from Hand Glove
-*/
-
-#include <SoftwareSerial.h>
-
-//Pin config for the HC-05 module
-#define RxD 6
-#define TxD 7 
-
-SoftwareSerial BTSerial(RxD, TxD);
-
-const uint8_t xPin   = A0; //Pin for Input from x-axis of accelerometer
-const uint8_t yPin   = A1; //Pin for Input from y-axis of accelerometer
-const uint8_t zPin   = A2; //Pin for Input from z-axis of accelerometer
-const uint8_t flex1 = A15; //Taking Input from flex of first finger 
-const uint8_t flex2 = A8; //Taking Input from flex of middle finger
-
+const int xPin   = A0; //Pin for Input from x-axis of accelerometer
+const int yPin   = A1; //Pin for Input from y-axis of accelerometer
+const int zPin   = A2; //Pin for Input from z-axis of accelerometer
+int flex1 = A15; //Taking Input from flex of first finger 
+int flex2 = A8; //Taking Input from flex of middle finger
 void setup() {
   //setting mode for pins
   pinMode(xPin,INPUT);
@@ -23,40 +10,32 @@ void setup() {
   pinMode(zPin,INPUT);
   pinMode(flex1, INPUT);
   pinMode(flex2, INPUT);
-
-	BTSerial.flush();
-	delay(500);
-
-	BTSerial.begin(9600);
-
-  Serial.begin(9600);
-  delay(100);
+   Serial.begin(9600);
 }
-
-void loop() {
-  int32_t x = analogRead(xPin); //Reading Input from x-axis of accelerometer
-  int32_t y = analogRead(yPin); //Reading Input from y-axis of accelerometer
-  int32_t z = analogRead(zPin); //Reading Input from z-axis of accelerometer
-  int32_t f1 = analogRead(flex1); //Reading Input from flex sensor on first finger
-  int32_t f2 = analogRead(flex2); //Reading Input from flex sensor on middle finger
+ void loop() {
+  int x = analogRead(xPin); //Reading Input from x-axis of accelerometer
+  int y = analogRead(yPin); //Reading Input from y-axis of accelerometer
+  int z = analogRead(zPin); //Reading Input from z-axis of accelerometer
+  int f1 = analogRead(flex1); //Reading Input from flex sensor on first finger
+  int f2 = analogRead(flex2); //Reading Input from flex sensor on middle finger
   
   if(y>370){
-    BTSerial.write("L"); //Printing L to move mouse pointer to left
+    Serial.println("L"); //Printing L to move mouse pointer to left
   }
   else if(y<300){
-    BTSerial.write("R"); //Printing R to move mouse pointer to right
+    Serial.println("R"); //Printing R to move mouse pointer to right
   }
   else if(x>370){
-    BTSerial.write("U"); //Printing U to move mouse pointer to up
+    Serial.println("U"); //Printing U to move mouse pointer to up
   }
   else if(x<300){
-    BTSerial.write("D"); //Printing D to move mouse pointer to down
+    Serial.println("D"); //Printing D to move mouse pointer to down
   }
   else if(f1>300){
-    BTSerial.write("C"); //Printing C to left click
+    Serial.println("C"); //Printing C to left click
   }
   else if(f2>150){
-    BTSerial.write("E"); //Printing E to right click
+    Serial.println("E"); //Printing E to right click
   }
   delay(100);
 }
--- Glove Pointer/mouse.py
+++ Glove Pointer/mouse.py
@@ -1,3 +1,32 @@
+# import serial
+# import pyautogui
+#  ser = serial.Serial('/dev/ttyACM0', 9600) #Connecting to the Arduino by giving it's port address
+#  #counters to make sure user actually wants to click
+# count1 = 0 
+# count2 = 0
+#  while True:
+#     a = ser.readline() #Reading what Arduino code is printing on serial monitor
+#     b= str(a)
+#     print(b[2])
+#     #Performing operation using pyautogui library according to character printed on serial monitor
+#     if b[2] == "D" :  
+#         pyautogui.moveRel(0, 10, duration = 0.01) 
+#     if b[2] == "R" :
+#         pyautogui.moveRel(10, 0, duration = 0.01)  
+#     if b[2] == "U" :
+#         pyautogui.moveRel(0, -10, duration = 0.01) 
+#     if b[2] == "L" :
+#         pyautogui.moveRel(-10,0, duration = 0.01)
+#     if b[2] == "C" :
+#         count1= count1+1
+#         if(count1==10): #clicking only when we get character corresponding to click 10 times to make sure user actually wants to click
+#             pyautogui.click()
+#             count1=0
+#     if b[2] == "E" :
+#         count2=  count2 + 1
+#         if(count2==10): #clicking only when we get character corresponding to click 10 times to make sure user actually wants to click
+#             pyautogui.click(button='right')
+#             count2=0
 import bluetooth
 import sys
 import pyautogui
